ea35283b8181b7ae062c3bf325641b8ba0ee1903
Cyber-Crime-tracker <76731045+Cyber-Crime-tracker@users.noreply.github.com>
2021-01-25 14:30:31 +0530

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -2,7 +2,7 @@ function ss(){
 			var a=document.getElementById("demo").value;
 			var b=document.getElementById("demo2").value;
 			if(a=="CSG57309" && b=="192.168.72.97"){
-				document.getElementById("demo1").innerHTML="<audio controls><source src=call/sandeep.mp3 type=audio/mp3></audio><div><p style=color:blue>Call tracked by K.AFREEN (Network Hacker)</p></div>";
+				document.getElementById("demo1").innerHTML="<audio controls><source src=call/sandeep.mp3 type=audio/mp3></audio><div><p style=color:blue>Call tracked by K.AFREEN (Network Hacker)</p><br><p style=color:blue>Tracked Date: 14/07/2019</p></div>";
 				
 			
 			}
