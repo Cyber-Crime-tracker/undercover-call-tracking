d4d88c85137295b9438c56eeb3c638d4ddcfddb2
Cyber-Crime-tracker <76731045+Cyber-Crime-tracker@users.noreply.github.com>
2021-01-28 22:27:36 +0530

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -63,7 +63,10 @@ function ss(){
 			
 				document.getElementById("demo1").innerHTML="<audio controls controlsList=nodownload><source src=call/panu.mp3 type=audio/mp3></audio><div style=text-align:left;padding-left:200px><p>Call tracked by <p style=font-weight:bold>K.AFREEN (Network Hacker)</p></p><br><p>Tracked Date: 18/12/2019</p><br><p>Type: Incoming</p><br><p>Duration: 00:02:04</p></div>";
 			}
+			else if(a=="12E26803" && b=="192.168.23.97"){
 			
+				document.getElementById("demo1").innerHTML="<audio controls controlsList=nodownload><source src=call/panu2.mp3 type=audio/mp3></audio><div style=text-align:left;padding-left:200px><p>Call tracked by <p style=font-weight:bold>K.AFREEN (Network Hacker)</p></p><br><p>Tracked Date: 27/01/2020</p><br><p>Type: Outgoing</p><br><p>Duration: 00:02:04</p></div>";
+			}
 			else{
 			document.getElementById("demo1").innerHTML="<p style=color:red;>Enter Correct Recorder IP & ID to Track Audio</p>";
 			}
