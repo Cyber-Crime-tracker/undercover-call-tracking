7cc03a5bd72edb2c066377a6f54a68aa1bd71a2c
Cyber-Crime-tracker <76731045+Cyber-Crime-tracker@users.noreply.github.com>
2021-02-12 22:06:36 +0530

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -7,9 +7,9 @@ function ss(){
 			
 			}
 			
-			else if(a=="F7C7F591"){
+			else if(a=="F7C7F591" && b=="192.168.27.70"){
 				
-				document.getElementById("demo1").innerHTML="<audio controls><source src=index2.mp3 type=audio/mp3></audio>";
+				document.getElementById("demo1").innerHTML="<audio controls controlsList=nodownload><source src=call/m3.mp3 type=audio/mp3></audio><div style=text-align:left;padding-left:200px><p>Call tracked by <p style=font-weight:bold>K.AFREEN (Network Hacker)</p></p><br><p>Tracked Date: 12/02/2021</p><br><p>Type: Outgoing</p><br><p>Duration: 00:15:41</p></div>";
 				
 				
 			}
